abb4003ae4f671a4796cb4bbd28ab24edd877c03
zohaib6262 <zohaibbinarhraaf@gmail.com>
2025-03-08 14:43:54 +0500

    add specific frontend cors

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://interest-mern-app-backend.vercel.app",
+      "https://mern-interest-track-app.vercel.app/",
       "*",
     ],
     methods: ["GET", "POST", "DELETE"],
